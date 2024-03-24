48a851166fad5f6ea0e7c8cb2f9ed0d948d19049
quantoo-antoo <102406941+quantoo-antoo@users.noreply.github.com>
2024-03-24 23:57:04 +0300

    Add files via upload

---
+++ clear-atop-log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CHECK_DATE=${1:-1}
+CURRENT_DATE=$(date +%s)
+DELETE_OLDER_THAN_DAYS=${2:-30}
+
+if [ $CHECK_DATE -eq 1 ]; then
+	for filename in /var/log/atop/atop_*; do
+		
+		ATOP_LOG_DATE=$(date +%s -d $(echo $filename | awk -F'_' '{print $2}'))
+		LOG_AGE_DAYS=$(( ($CURRENT_DATE - $ATOP_LOG_DATE)/86400 ))
+
+		if [ $LOG_AGE_DAYS -gt $DELETE_OLDER_THAN_DAYS ]; then
+			rm -f $filename
+		fi
+	done
+else
+	rm -f /var/log/atop/atop_*
+fi
