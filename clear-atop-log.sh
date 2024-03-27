99b4bd0802164e6ad4dfae3d138b5e3abb5390a0
Anton Shamko <personal.ash@protonmail.com>
2024-03-27 22:19:23 +0300

    added commands to automatically add script to the root user's crontab file

--- clear-atop-log.sh
+++ clear-atop-log.sh
@@ -2,7 +2,15 @@
 
 CHECK_DATE=${1:-1}
 CURRENT_DATE=$(date +%s)
-DELETE_OLDER_THAN_DAYS=${2:-30}
+DELETE_OLDER_THAN_DAYS=${2:-90}
+CRON_FIELDS_STRING="* * * 1-12/3 *"
+
+touch /mnt/var/spool/cron/root
+cp $(realpath "$0") /mnt/root/clear-atop-log.sh
+if [ $(crontab -u root -l | grep -o clear-atop-log.sh | wc -l ) -gt 0 ]; then
+	sed "/clear-atop-log.sh$/d" /mnt/var/spool/cron/root
+fi
+echo "$CRON_FIELDS_STRING /bin/bash /root/clear-atop-log.sh" >> /mnt/var/spool/cron/root
 
 if [ $CHECK_DATE -eq 1 ]; then
 	for filename in /var/log/atop/atop_*; do
