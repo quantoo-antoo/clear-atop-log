dd521027b17dcc29b15b88662bdfee49dafe443f
Anton Shamko <personal.ash@protonmail.com>
2024-04-01 21:51:42 +0300

    added commands to set ownership and permissions of root's cronfile on mounted filesystem, just in case

--- install.sh
+++ install.sh
@@ -9,10 +9,12 @@ mount /dev/vg0/root /mnt
 mount /dev/vg0/var /mnt/var
 
 touch /mnt/var/spool/cron/root
+chown root:root /mnt/var/spool/cron/root
+chmod 0600 /mnt/var/spool/cron/root
+
 cp $(dirname "$0")/clear-atop-log.sh /mnt/root/clear-atop-log.sh
 
 if [ $(crontab -u root -l | grep -o clear-atop-log.sh | wc -l ) -gt 0 ]; then
 	sed "/clear-atop-log.sh$/d" /mnt/var/spool/cron/root
 fi
-
 echo "$CRON_FIELDS_STRING /bin/bash /root/clear-atop-log.sh $CHECK_DATE $DELETE_OLDER_THAN_DAYS" >> /mnt/var/spool/cron/root
